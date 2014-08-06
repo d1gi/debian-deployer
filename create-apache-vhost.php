956bdc35692db46b1ce9925312be7db8671e1f45
d1gi <digital_boy@mail.ru>
2014-08-06 22:09:46 +0700

    fix

--- create-apache-vhost.php
+++ create-apache-vhost.php
@@ -33,5 +33,5 @@ $conf = "
 
 file_put_contents('/etc/apache2/sites-enabled/' . $domain, $conf);
 
-mkdir('/var/www/test1.deb');
-mkdir('/var/www/test1.deb/web');
+mkdir("/var/www/{$domain}.deb");
+mkdir("/var/www/{$domain}.deb/web");
