76eb13e59b22bee7acec30e60a1005232b1418d6
d1gi <digital_boy@mail.ru>
2014-08-27 21:14:43 +0700

    add warmup-sites

---
+++ new/usr/local/bin/warmup-sites.php
@@ -0,0 +1,11 @@
+#!/usr/bin/env php
+<?php
+// */5 * * * * root /usr/local/bin/warmup-sites.php &> /dev/null
+
+$directory = '/var/www';
+
+foreach (array_diff(scandir($directory), ['..', '.']) as $dir) {
+    if (is_dir($directory . '/' . $dir)) {
+        system("curl http://{$dir} > /dev/null");
+    }
+}
