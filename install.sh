afc0efdae9381e882c08b112c457a80733710596
d1gi <digital_boy@mail.ru>
2014-08-27 10:20:41 +0700

    imagemagick

--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ wget -qO- https://deb.nodesource.com/setup_nodesource_repo | bash -
 
 # Базовый софт
 apt-get install colordiff mc make htop make git curl rcconf p7zip-full zip ruby ruby-dev dnsutils monit python-software-properties -y
-apt-get install locales locales-all fail2ban resolvconf subversion ntp -y
+apt-get install locales locales-all fail2ban resolvconf subversion ntp imagemagick -y
 apt-get install libedit-dev automake1.1 libncurses-dev libpcre3-dev pkg-config python-docutils -y
 apt-get install oracle-java7-installer -y
 #apt-get install elasticsearch -y
